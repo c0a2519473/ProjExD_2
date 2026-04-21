c3e376510232f7abfe129e077dd0563e74f213e8
kota <c0a2519473@edu.teu.ac.jp>
2026-04-21 14:23:10 +0900

    練習２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import pygame as pg
-
+import random
 
 WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -14,6 +14,12 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img,(250,0,0),(10,10),10)
+    bb_img.set_colorkey((0,0,0))
+    bb_rct=bb_img.get_rect()
+    bb_rct.centerx=random.randint(0,WIDTH)
+    bb_rct.centery=random.randint(0,HEIGHT)
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -22,6 +28,15 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
+        
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         DELTA={pg.K_UP:(0,-5),
@@ -34,14 +49,12 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
-        # if key_lst[pg.K_UP]:
-        #     sum_mv[1] -= 5
-        # if key_lst[pg.K_DOWN]:
-        #     sum_mv[1] += 5
-        # if key_lst[pg.K_LEFT]:
-        #     sum_mv[0] -= 5
-        # if key_lst[pg.K_RIGHT]:
-        #     sum_mv[0] += 5
+
+        vx=+5 #横方向速度 
+        vy=+5 #縦方向速度
+        
+        bb_rct.move_ip(vx,vy)
+        screen.blit(bb_img,bb_rct)
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
