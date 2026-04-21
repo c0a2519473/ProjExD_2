f4deacc2cb123081a600c56d5c2e3e2c0e07658b
kota <c0a2519473@edu.teu.ac.jp>
2026-04-21 17:07:17 +0900

    時間で拡大するやつ途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -38,6 +38,16 @@ def gameover(screen:pg.Surface) -> None:
     bg_black.blit(kc_img,kc_rct)
     screen.blit(bg_black,(0,0))
 
+def init_bb_imgs()->tuple[list[pg.Surface],list[int]]:
+    bb_imgs=[]
+    for r in range(1,11):
+        bb_img =pg.Surface((20*r,20*r))
+        pg.draw.circle(bb_img,(255,0,0),(10*r,10*r),10*r)
+        bb_imgs.append(bb_img)
+        bb_accs=[a for a in range(1,11)]
+    return bb_imgs,bb_accs
+
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -56,6 +66,8 @@ def main():
     vy=+5 #縦方向速度
     clock = pg.time.Clock()
     tmr = 0
+    init_bb_imgs()
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -96,14 +108,23 @@ def main():
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
 
         screen.blit(kk_img, kk_rct)
+        
+        avx=vx*bb_accs[min(tmr//500,9)]
+        avy=vy*bb_accs[min(tmr//500,9)]
+        bb_img = bb_imgs[min(tmr//500, 9)]
+        
 
-        bb_rct.move_ip(vx,vy)
+        bb_rct.move_ip(avx,avy)
         yoko,tate=check_bound(bb_rct)
         if not yoko:
             vx *= -1
         if not tate:
             vy *= -1
 
+
+        bb_rct.width=bb_img.get_rect().width
+
+        
         
         screen.blit(bb_img,bb_rct)
         pg.display.update()
